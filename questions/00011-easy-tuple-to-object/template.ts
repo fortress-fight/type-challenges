8253d52b3f73062437b68c15c7bc00ab6bd34c33
f-stone <dreamersfly@163.com>
2022-04-28 14:26:13 +0800

    feature(03): 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,5 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly string[]> = {
+  [P in T[number]]: P
+}
+const myTuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+let test: TupleToObject<typeof myTuple>
