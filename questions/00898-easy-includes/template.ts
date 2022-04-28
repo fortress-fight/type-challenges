739a25887f5ee6c8fa8d05f4155238c8a43669c8
f-stone <dreamersfly@163.com>
2022-04-28 16:56:03 +0800

    feature(10): 00898-easy-includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,15 @@
-type Includes<T extends readonly any[], U> = any
+// 关于如何实现 Equal 的讨论：https://github.com/microsoft/TypeScript/issues/27024
+
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Other,
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Other, U>
+  : false
